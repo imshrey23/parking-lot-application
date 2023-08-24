02f2155cbd80fe4b8bff564ed2d517589ad012e8
imshrey23 <dhumes@oregonstate.edu>
2023-08-24 14:45:39 -0700

    Completed code refactoring

--- app/src/main/java/com/example/msproject/api/apiService/ApiConstant.kt
+++ app/src/main/java/com/example/msproject/api/apiService/ApiConstant.kt
@@ -2,7 +2,7 @@ package com.example.msproject.api.apiService
 
 object ApiConstant {
     const val PARKING_LOTS_API = "https://658ytxfrod.execute-api.us-east-1.amazonaws.com/dev/parking_lots"
-    const val PARKING_LOT_API = "http://10.0.2.2:8000/parkingLots/:parkingLotName"
+    const val PARKING_LOT_API = "http://10.0.2.2:8000/parkingLot/"
     const val UPDATE_PARKING_LOT = "http://10.0.2.2:8000/reserveParkingSpot"
     const val DISTANCE_MATRIX = "https://maps.googleapis.com/maps/api/distancematrix/json?"
 }
